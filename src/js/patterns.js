5dec36a071034c35c004a8feccc136bf83c7fc28
Nadim Kobeissi <nadim@nadim.computer>
2016-04-08 17:19:26 -0400

    Fix typo introduced in previous commit.

--- src/js/patterns.js
+++ src/js/patterns.js
@@ -1,6 +1,6 @@
 Cryptocat.Patterns = {
 	username:   /^([a-z0-9]|_){1,16}$/,
-	password:   /^.{13,512}$/,
+	password:   /^.{12,512}$/,
 	version:    /^(\d){1,2}.(\d){1,2}.(\d){1,2}$/,
 	sticker:    /^CryptocatSticker:\w{4,20}$/,
 	file:       /^CryptocatFile:.{240,512}$/,
