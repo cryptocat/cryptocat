a6ecf71a9fa97a3dccc59e7467541cdf58899a34
Nadim Kobeissi <nadim@nadim.computer>
2016-04-19 02:41:01 +0200

    Fix #67.

--- src/app.js
+++ src/app.js
@@ -272,6 +272,36 @@ var buildMainMenu = function(settings) {
 			]
 		},
 		{
+			label: 'Edit',
+			submenu: [{
+				label: 'Undo',
+				accelerator: 'CmdOrCtrl+Z',
+				role: 'undo'
+			}, {
+				label: 'Redo',
+				accelerator: 'Shift+CmdOrCtrl+Z',
+				role: 'redo'
+			}, {
+				type: 'separator'
+			}, {
+				label: 'Cut',
+				accelerator: 'CmdOrCtrl+X',
+				role: 'cut'
+			}, {
+				label: 'Copy',
+				accelerator: 'CmdOrCtrl+C',
+				role: 'copy'
+			}, {
+				label: 'Paste',
+				accelerator: 'CmdOrCtrl+V',
+				role: 'paste'
+			}, {
+				label: 'Select All',
+				accelerator: 'CmdOrCtrl+A',
+				role: 'selectall'
+			}]
+		},
+		{
 			label: 'Help',
 			role: 'help',
 			submenu: [{
