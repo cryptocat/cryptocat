d8c92687873aca6974a4730f0537fb23e7996cbf
Nadim Kobeissi <nadim@nadim.computer>
2016-04-21 02:12:15 +0200

    Cryptocat 3.1.05

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,4 @@
-const VERSION = '3.1.04';
+const VERSION = '3.1.05';
 
 module.exports = function(grunt) {
 	grunt.initConfig({
--- src/js/version.js
+++ src/js/version.js
@@ -1,1 +1,1 @@
-Cryptocat.Version = '3.1.04';
+Cryptocat.Version = '3.1.05';
